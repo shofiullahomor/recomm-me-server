7301a4d7f19d8297973993b398f1fc36fca4c89e
shofiullah omor <shofiullahomor1436@gmail.com>
2025-02-17 14:43:59 +0600

    delete api added

--- index.js
+++ index.js
@@ -39,6 +39,13 @@ async function run() {
       const result = await queriesCollection.find(query).toArray();
       res.send(result);
     });
+    // delete a query from database
+    app.delete("/query/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await queriesCollection.deleteOne(query);
+      res.send(result);
+    });
     //get single query
     app.get("/queries/:id", async (req, res) => {
       const id = req.params.id;
